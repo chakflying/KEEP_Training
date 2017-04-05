2aa0866fe9b4d8495e552091bae1f8c16636d848
Johnson Ma <jabber484@gmail.com>
2017-04-05 15:20:18 +0800

    RESTART: Simple program

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int swt, flag = 1;
+	
+	printf("Git is cool!\n Enter your function number:\n");
+	while(flag)
+	scanf("%d",&swt);
+	
+	swtich(swt):
+	case 0: flag = 0;
+			break;
+	case 1: one();
+			break;
+	
+	return 0;
+}
